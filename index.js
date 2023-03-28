64e734177a6a10045c571ef2323fa410f6d68ed4
henry <henry.fan@softtek.com>
2023-03-28 09:52:12 +0800

    update

--- index.js
+++ index.js
@@ -23,14 +23,8 @@ app.use('/', express.static('public'));
 app.use('/chat', express.static('public/chat.html'));
 
 app.use(session({
-  secret: 'this is string key',   // 可以随便写。 一个 String 类型的字符串，作为服务器端生成 session 的签名
-  name: 'session_id',/*保存在本地cookie的一个名字 默认connect.sid  可以不设置*/
-  resave: false,   /*强制保存 session 即使它并没有变化,。默认为 true。建议设置成 false。*/
-  saveUninitialized: true,   //强制将未初始化的 session 存储。  默认值是true  建议设置成true
-  cookie: {
-    // maxAge: 3600    /*过期时间*/
-  },
-  rolling: true //在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
+  secret: 'chat-bot',   // 可以随便写。 一个 String 类型的字符串，作为服务器端生成 session 的签名
+  resave: false   /*强制保存 session 即使它并没有变化,。默认为 true。建议设置成 false。*/
 }))
 
 const loggerMiddleware = require('./src/middleware/logger')
