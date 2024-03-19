f8c0e082c71f2395eccccaa5eb78a56c84fd4cb9
henry <henry.fan@softtek.com>
2024-03-19 10:28:36 +0800

    update

--- src/service/openAI.js
+++ src/service/openAI.js
@@ -1,21 +1,21 @@
 'use strict'
-const {Configuration, OpenAIApi} = require("openai");
+const { OpenAI } = require("openai");
 
 // const configuration = new Configuration({
 //   apiKey: process.env.OPENAI_API_KEY,
 // });
-const openai = new OpenAIApi({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 let data = []
 const getOpenAiReply = async (messages) => {
   // const response = await openai.createChatCompletion({
-  const response = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages
   })
   return response.data.choices[0].message
 }
 
-module.exports = {getOpenAiReply}
+module.exports = { getOpenAiReply }
